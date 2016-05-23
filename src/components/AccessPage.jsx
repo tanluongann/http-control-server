a013c90b7b957fcfc51690285a3b8bd1d0ff92c4
Jeremy Tan <tanluongann@gmail.com>
2016-05-24 00:36:27 +0800

    Add login box

--- src/components/AccessPage.jsx
+++ src/components/AccessPage.jsx
@@ -8,23 +8,21 @@ export const AccessPage = React.createClass({
 
   render: function() {
 
-   return <div className="accesspage">
+   	return <div className="accesspage">
+		<div className="box">
 
-	<div className="box">
+			<div className="panel1">
+				<h2>Welcome to</h2>
+				<h2>UJustWatch</h2>
+			</div>
 
-		<div className="panel1">
-			<h2>Welcome to</h2>
-			<h2>UJustWatch</h2>
+			<div className="panel2">
+				loginpage
+			</div>
+
 		</div>
-		
-		<div className="panel2">
-			loginpage
-		</div>
+	</div>
 
-  </div>
-</div>
-
-   </div>;
   }
 
 });
