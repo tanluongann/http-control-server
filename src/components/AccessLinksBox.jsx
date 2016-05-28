9dc250d47d783018d45ca820f32488365b0ca86f
Jeremy Tan <tanluongann@gmail.com>
2016-05-29 02:25:39 +0800

    Improve display

--- src/components/AccessLinksBox.jsx
+++ src/components/AccessLinksBox.jsx
@@ -10,43 +10,43 @@ export const AccessLinksBox = React.createClass({
   render: function() {
     var activeClass = this.props.visible ? 'accesslinksbox' : 'accesslinksbox hidden';
 
-    var content = 'No device info';
-    if (this.props.device) {
-      var links = {
-        "sonarr": "http://"+this.props.device.ip+":8989/calendar",
-        "couchpotato": "http://"+this.props.device.ip+":5050",
-        "transmission": "http://"+this.props.device.ip+":9091",
-        "kodi": "http://"+this.props.device.ip+":8080",
-      }
-      content = <div><span className="instructions">Select the service you want to open</span>
-        <ul className="links">
-          <li>
-            <a href={ links['sonarr'] } target="_blank">
-              <i className="fa fa-fw fa-calendar-check-o"></i>
-              <span className="name">TV Shows</span>
-            </a>
-          </li>
-          <li>
-            <a href={ links['couchpotato'] } target="_blank">
-              <i className="fa fa-fw fa-ticket"></i>
-              <span className="name">Movies</span>
-            </a>
-          </li>
-          <li>
-            <a href={ links['transmission'] } target="_blank">
-              <i className="fa fa-fw fa-cloud-download"></i>
-              <span className="name">Downloads</span>
-            </a>
-          </li>
-          <li>
-            <a href={ links['kodi'] } target="_blank">
-              <i className="fa fa-fw fa-television"></i>
-              <span className="name">TV Player</span>
-            </a>
-          </li>
-        </ul>
-      </div>
+    var device = this.props.device;
+    if (!device) device = { ip: "127.0.0.1", empty: true}
+    var links = {
+      "sonarr": "http://"+device.ip+":8989/calendar",
+      "couchpotato": "http://"+device.ip+":5050",
+      "transmission": "http://"+device.ip+":9091",
+      "kodi": "http://"+device.ip+":8080",
     }
+    content = <div className={ device.empty ? 'empty', '' } >
+      <span className="instructions">Select the service you want to open</span>
+      <ul className="links">
+        <li>
+          <a href={ links['sonarr'] } target="_blank">
+            <i className="fa fa-fw fa-calendar-check-o"></i>
+            <span className="name">TV Shows</span>
+          </a>
+        </li>
+        <li>
+          <a href={ links['couchpotato'] } target="_blank">
+            <i className="fa fa-fw fa-ticket"></i>
+            <span className="name">Movies</span>
+          </a>
+        </li>
+        <li>
+          <a href={ links['transmission'] } target="_blank">
+            <i className="fa fa-fw fa-cloud-download"></i>
+            <span className="name">Downloads</span>
+          </a>
+        </li>
+        <li>
+          <a href={ links['kodi'] } target="_blank">
+            <i className="fa fa-fw fa-television"></i>
+            <span className="name">TV Player</span>
+          </a>
+        </li>
+      </ul>
+    </div>
 
     return <div className={ activeClass }>
       { content }
